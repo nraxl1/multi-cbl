4d8dee44108a25ad3dbb04df3bdb13a287f0c6c0
nraxl1 <haha@idk.lol>
2026-05-17 00:44:06 +0200

    move into package

--- IRSpectraML/src/IRSpectraML.jl
+++ IRSpectraML/src/IRSpectraML.jl
@@ -1,5 +1,10 @@
 module IRSpectraML
 
+include_files = ["Featurization.jl", "LoadData.jl", "ModelNext.jl", "TrainingNext.jl"]
+for filename in include_files
+    include(dirname(@__FILE__) * filename) # asterisk here does string concat idk ask the language developers
+end
+
 greet() = print("Hello World!")
 
 end # module IRSpectraML
